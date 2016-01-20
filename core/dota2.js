de2c87ee4a06741c5651c3e334a1b8d4458f22f5
beatyt <beatyt@gmail.com>
2016-01-20 18:22:18 -0500

    Update node-steam and node-dota2 versions and the reference names to the APIs

--- core/dota2.js
+++ core/dota2.js
@@ -1,6 +1,8 @@
+var logger = require('./logger.js');
 var Dota2 = require('dota2');
 var dota2;
 var inGame = false;
+
 
 exports.init = function(bot) {
   dota2 = new Dota2.Dota2Client(bot, true);
@@ -10,6 +12,22 @@ exports.launch = function() {
   if (!inGame) {
     dota2.launch();
     inGame = true;
+
+    dota2.on('ready', function() {
+      logger.log('Dota2 is ready to do things.');
+    });
+
+    dota2.on('partyInviteUpdate', function(party) {
+	    console.log(party);
+      var partyId = party.group_id;
+      dota2.respondPartyInvite(partyId, true);
+      dota2.joinChat(partyId);
+    });
+
+    dota2.on('chatMessage', function(channel, senderName, message, chatObject) {
+      logger.log('[messaged receieved] ' + senderName +  ': ' + message + '. On ' + channel);
+	  logger.log(chatObject);
+    });
   }
 };
 
